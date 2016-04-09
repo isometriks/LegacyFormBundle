a0940e71e364824cf0be8d1a00a382b8532c293a
Kamil Kokot <kamil@kokot.me>
2016-04-09 15:03:24 +0200

    Move default form classes map to configuration file

---
+++ src/DependencyInjection/IsometriksLegacyFormExtension.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Isometriks\Bundle\LegacyFormBundle\DependencyInjection;
+
+use Symfony\Component\Config\FileLocator;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
+use Symfony\Component\HttpKernel\DependencyInjection\Extension;
+
+/**
+ * @author Kamil Kokot <kamil@kokot.me>
+ */
+final class IsometriksLegacyFormExtension extends Extension
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function load(array $configs, ContainerBuilder $container)
+    {
+        $loader = new XmlFileLoader(
+            $container,
+            new FileLocator(__DIR__ . '/../Resources/config')
+        );
+
+        $loader->load('services.xml');
+    }
+}
--- src/IsometriksLegacyFormBundle.php
+++ src/IsometriksLegacyFormBundle.php
@@ -3,13 +3,15 @@
 namespace Isometriks\Bundle\LegacyFormBundle;
 
 use Isometriks\Bundle\LegacyFormBundle\DependencyInjection\Compiler\FormRegistryCompilerPass;
+use Isometriks\Bundle\LegacyFormBundle\DependencyInjection\IsometriksLegacyFormExtension;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 /**
  * @author Craig Blanchette <craig.blanchette@gmail.com>
+ * @author Kamil Kokot <kamil@kokot.me>
  */
-class IsometriksLegacyFormBundle extends Bundle
+final class IsometriksLegacyFormBundle extends Bundle
 {
     /**
      * {@inheritdoc}
@@ -19,5 +21,13 @@ class IsometriksLegacyFormBundle extends Bundle
         parent::build($container);
 
         $container->addCompilerPass(new FormRegistryCompilerPass());
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getContainerExtension()
+    {
+        return new IsometriksLegacyFormExtension();
     }
 }
